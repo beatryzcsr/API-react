a2ede1110d6b86ab53746561c32a92042dae16ed
BeatrizOliveira15 <beatriz.s.oliveira15@aluno.senai.br>
2026-04-14 14:58:44 -0300

    jsx completo

--- api-react/src/App.jsx
+++ api-react/src/App.jsx
@@ -23,6 +23,46 @@ function App() {
       setLoading(false)
     }
   }
+  
+  return (
+    <div className="container">
+      <h1>Titulo</h1>
 
+<div className="info">
+        <strong>Cliente Servidor:</strong> O cliente (usuário) solicita algo e o servidor (máquina) processa e fornece a resposta
+      </div>
+
+
+      <button onClick={buscarDados} disabled={loading}>
+        {loading ? 'Carregando...' : 'Buscar Dados da API'}
+      </button>
+
+      <div className="resultado">
+        {loading && <em>Carregando informações...</em>}
+
+        {erro && (
+          <>
+          <h3>❌ Erro:</h3>
+          <p>{erro}</p>
+          </>
+        )}
+
+        {dados && !loading && (
+          <>
+          <h3>Dados Recebidos:</h3>
+          <p><strong>Nome:</strong> {dados.name}</p>
+          <p><strong>Data de Nascimento:</strong> {dados.birth_year}</p>
+          <p><strong>Altura:</strong> {dados.height}</p>
+          <p><strong>Cor:</strong> {dados.skin_color}</p>
+          </>
+        )}
+
+        {!dados && !loading && !erro && (
+          <em>Clique no botão para fazer uma requisição...</em>
+        )}
+      </div>
+    </div>
+  )
+}
 
 export default App;
